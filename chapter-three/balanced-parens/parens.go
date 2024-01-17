56bf70ae7b5b5e171be32d4818b02c0d852553c6
teleivo <teleivo@users.noreply.github.com>
2024-01-17 06:35:44 +0100

    exercise: solve 3.10 3-2

--- chapter-three/balanced-parens/parens.go
+++ chapter-three/balanced-parens/parens.go
@@ -20,7 +20,34 @@ func isBalanced(in string) (int, bool) {
 		openParens--
 	}
 
-	// TODO
 	// opening paren without an closing one
 	return 0, openParens == 0
+}
+
+// countLongestBalanced solves exercise 3.10 3-2.
+// Note: I am confused by the hint which to me seems to contradict the problem statement. Finding
+// the longest balanced parenthesis while it is not necessarily a contiguous run of parenthesis does
+// not make sense to me.
+func countLongestBalanced(in string) int {
+	var count int
+	var openParens int
+
+	for i := 0; i < len(in); i++ {
+		if in[i] == '(' {
+			openParens++
+			continue
+		}
+
+		// closing paren without an opening one
+		if openParens == 0 {
+			continue
+		}
+
+		// consume opening paren for closing one
+		openParens--
+		// count both parens
+		count += 2
+	}
+
+	return count
 }
--- chapter-three/balanced-parens/parens_test.go
+++ chapter-three/balanced-parens/parens_test.go
@@ -3,7 +3,6 @@ package parens
 import "testing"
 
 func TestBalancedParens(t *testing.T) {
-
 	tests := []struct {
 		in      string
 		wantPos int
@@ -46,6 +45,55 @@ func TestBalancedParens(t *testing.T) {
 
 		if gotPos != tc.wantPos || gotOk != tc.wantOk {
 			t.Errorf("isBalanced(%q) = (%d, %t) want (%d, %t)", tc.in, gotPos, gotOk, tc.wantPos, tc.wantOk)
+		}
+	}
+}
+
+func TestCountLongestBalanced(t *testing.T) {
+	tests := []struct {
+		in     string
+		want   int
+		wantOk bool
+	}{
+		{
+			in:   "",
+			want: 0,
+		},
+		{
+			in:   "()",
+			want: 2,
+		},
+		{
+			in:   "((())())()",
+			want: 10,
+		},
+		{
+			in:   ")",
+			want: 0,
+		},
+		{
+			in:   "(",
+			want: 0,
+		},
+		{
+			in:   "())",
+			want: 2,
+		},
+		{
+			in:   ")()(",
+			want: 2,
+		},
+		{
+			in:   ")()(())()()))())))(",
+			want: 12,
+		},
+	}
+
+	for _, tc := range tests {
+		got := countLongestBalanced(tc.in)
+
+		if got != tc.want {
+			t.Errorf("countLongestBalanced(%q) = %d want %d", tc.in, got, tc.want)
 		}
 	}
 }
