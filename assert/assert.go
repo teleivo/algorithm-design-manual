637039fd44b529534fd89d976d85e082cb13e0b2
teleivo <teleivo@users.noreply.github.com>
2024-01-26 05:20:06 +0100

    test: adapt signature

--- assert/assert.go
+++ assert/assert.go
@@ -12,13 +12,13 @@ func NoError(t *testing.T, err error) {
 	report.NoError(t.Errorf, err)
 }
 
-func False(t *testing.T, method string, got bool) {
+func False(t *testing.T, got bool) {
 	t.Helper()
 
 	report.False(t.Errorf, got)
 }
 
-func True(t *testing.T, method string, got bool) {
+func True(t *testing.T, got bool) {
 	t.Helper()
 
 	report.True(t.Errorf, got)
@@ -30,13 +30,13 @@ func Nil(t *testing.T, got any) {
 	report.Nil(t.Errorf, got)
 }
 
-func Equals(t *testing.T, method string, got, want int) {
+func Equals(t *testing.T, got, want int) {
 	t.Helper()
 
 	report.Equals(t.Errorf, got, want)
 }
 
-func EqualValues(t *testing.T, method string, in, got, want any) {
+func EqualValues(t *testing.T, got, want any) {
 	t.Helper()
 
 	report.EqualValues(t.Errorf, got, want)
