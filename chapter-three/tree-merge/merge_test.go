9970410c9b71a34954969924a35b67def5e4a6cc
teleivo <teleivo@users.noreply.github.com>
2024-02-15 06:25:13 +0100

    test: use same tests for all implementations

--- chapter-three/tree-merge/merge_test.go
+++ chapter-three/tree-merge/merge_test.go
@@ -6,129 +6,72 @@ import (
 	"github.com/teleivo/algorithm-design-manual/assert"
 )
 
-func TestMerge(t *testing.T) {
-	t.Skip()
-	t.Run("OnlyLeftTree", func(t *testing.T) {
-		n1 := New(2, 1, 3)
-		got := &list{}
+func TestMergeImplementations(t *testing.T) {
+	implementations := map[string]func(*node, *node, *list){
+		"mergeIter": mergeIter,
+	}
 
-		merge(n1, nil, got)
+	for name, f := range implementations {
+		t.Run(name+"/OnlyLeftTree", func(t *testing.T) {
+			n1 := New(2, 1, 3)
+			got := &list{}
 
-		want := []int{
-			1, 2, 3,
-		}
-		assertList(t, got, want)
-	})
-	t.Run("LeftTreeSmallerThanRight", func(t *testing.T) {
-		n1 := New(2, 1, 3)
-		n2 := New(5, 4, 6)
-		got := &list{}
+			f(n1, nil, got)
 
-		merge(n1, n2, got)
+			want := []int{
+				1, 2, 3,
+			}
+			assertList(t, got, want)
+		})
+		t.Run(name+"/LeftTreeSmallerThanRight", func(t *testing.T) {
+			n1 := New(2, 1, 3)
+			n2 := New(5, 4, 6)
+			got := &list{}
 
-		want := []int{
-			1, 2, 3, 4, 5, 6,
-		}
-		assertList(t, got, want)
-	})
-	t.Run("RightTreeSmallerThanLeft", func(t *testing.T) {
-		n1 := New(5, 4, 6)
-		n2 := New(2, 1, 3)
-		got := &list{}
+			f(n1, n2, got)
 
-		merge(n1, n2, got)
+			want := []int{
+				1, 2, 3, 4, 5, 6,
+			}
+			assertList(t, got, want)
+		})
+		t.Run(name+"/RightTreeSmallerThanLeft", func(t *testing.T) {
+			n1 := New(5, 4, 6)
+			n2 := New(2, 1, 3)
+			got := &list{}
 
-		want := []int{
-			1, 2, 3, 4, 5, 6,
-		}
-		assertList(t, got, want)
-	})
-	t.Run("EqualValues", func(t *testing.T) {
-		n1 := New(2, 1, 4)
-		n2 := New(2, 4, 6)
-		got := &list{}
+			f(n1, n2, got)
 
-		merge(n1, n2, got)
+			want := []int{
+				1, 2, 3, 4, 5, 6,
+			}
+			assertList(t, got, want)
+		})
+		t.Run(name+"/EqualValues", func(t *testing.T) {
+			n1 := New(2, 1, 4)
+			n2 := New(2, 4, 6)
+			got := &list{}
 
-		want := []int{
-			1, 2, 4, 6,
-		}
-		assertList(t, got, want)
-	})
-	t.Run("Mixed", func(t *testing.T) {
-		n1 := New(11, 7, 4, 8, 24, 1, 6)
-		n2 := New(3, 22, 2, 9, 5, 36)
-		got := &list{}
+			f(n1, n2, got)
 
-		merge(n1, n2, got)
+			want := []int{
+				1, 2, 4, 6,
+			}
+			assertList(t, got, want)
+		})
+		t.Run(name+"/Mixed", func(t *testing.T) {
+			n1 := New(11, 7, 4, 8, 24, 1, 6)
+			n2 := New(3, 22, 2, 9, 5, 36)
+			got := &list{}
 
-		want := []int{
-			1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 24, 36,
-		}
-		assertList(t, got, want)
-	})
-}
+			f(n1, n2, got)
 
-func TestMergeIter(t *testing.T) {
-	t.Run("OnlyLeftTree", func(t *testing.T) {
-		n1 := New(2, 1, 3)
-		got := &list{}
-
-		mergeIter(n1, nil, got)
-
-		want := []int{
-			1, 2, 3,
-		}
-		assertList(t, got, want)
-	})
-	t.Run("LeftTreeSmallerThanRight", func(t *testing.T) {
-		n1 := New(2, 1, 3)
-		n2 := New(5, 4, 6)
-		got := &list{}
-
-		mergeIter(n1, n2, got)
-
-		want := []int{
-			1, 2, 3, 4, 5, 6,
-		}
-		assertList(t, got, want)
-	})
-	t.Run("RightTreeSmallerThanLeft", func(t *testing.T) {
-		n1 := New(5, 4, 6)
-		n2 := New(2, 1, 3)
-		got := &list{}
-
-		mergeIter(n1, n2, got)
-
-		want := []int{
-			1, 2, 3, 4, 5, 6,
-		}
-		assertList(t, got, want)
-	})
-	t.Run("EqualValues", func(t *testing.T) {
-		n1 := New(2, 1, 4)
-		n2 := New(2, 4, 6)
-		got := &list{}
-
-		mergeIter(n1, n2, got)
-
-		want := []int{
-			1, 2, 4, 6,
-		}
-		assertList(t, got, want)
-	})
-	t.Run("Mixed", func(t *testing.T) {
-		n1 := New(11, 7, 4, 8, 24, 1, 6)
-		n2 := New(3, 22, 2, 9, 5, 36)
-		got := &list{}
-
-		mergeIter(n1, n2, got)
-
-		want := []int{
-			1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 24, 36,
-		}
-		assertList(t, got, want)
-	})
+			want := []int{
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 24, 36,
+			}
+			assertList(t, got, want)
+		})
+	}
 }
 func assertList(t *testing.T, got *list, want []int) {
 	var result []int
