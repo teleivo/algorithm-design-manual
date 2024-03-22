da7b93beda9a802fa0f6b367f67fdc304f4d2b4c
teleivo <teleivo@users.noreply.github.com>
2024-03-22 06:41:55 +0100

    exercise: solve 3.10 3-25 part one

--- chapter-three/binpacking/binpacking.go
+++ chapter-three/binpacking/binpacking.go
@@ -1,2 +1,141 @@
 // Package binpacking solves exercise 3.10 3-25.
 package binpacking
+
+type node struct {
+	Value uint
+	Left  *node
+	Right *node
+}
+
+func New(values ...uint) *node {
+	var root *node
+
+	for _, v := range values {
+		root = insert(root, v)
+	}
+
+	return root
+}
+
+// FindBestFit returns the number of bins needed to fit objects of given size using the best-fit
+// heuristic.
+// FindBestFit solves exercise 3.10 3-25.
+// Time: O(N log N). For each of the objects a couple of O(log N) operations are needed.
+// FindWorstFit can be implemented in the same way replacing findSmallesLargerThan with
+// findLargestLarerThan. So instead of going left on a bin with enough space go right.
+func FindBestFit(objects []uint, capacity uint) uint {
+	var binCount uint
+	bins := New()
+
+	for _, object := range objects {
+		bin := findSmallestLargerThan(bins, object)
+		if bin == nil {
+			bins = insert(bins, capacity-object)
+			binCount++
+		} else {
+			bins = deleteNode(bins, bin.Value)
+			bins = insert(bins, bin.Value-object)
+		}
+	}
+
+	return binCount
+}
+
+func findSmallestLargerThan(n *node, value uint) *node {
+	return findSmallestLargerThanRecur(n, value, nil)
+}
+
+func findSmallestLargerThanRecur(n *node, value uint, candidate *node) *node {
+	if n == nil {
+		return candidate
+	}
+	if n.Value >= value {
+		// fits so we have a candidate but there might be a better-fit i.e. a smaller one that
+		// fits as well
+		return findSmallestLargerThanRecur(n.Left, value, n)
+	}
+	return findSmallestLargerThanRecur(n.Right, value, candidate)
+}
+
+func deleteNode(n *node, value uint) *node {
+	if n == nil {
+		return n
+	}
+
+	if n.Value == value {
+		return findSuccessor(n)
+	}
+
+	if n.Left != nil && n.Right == nil && n.Left.Value == value {
+		n.Left = findSuccessor(n.Left)
+		return n
+	} else if n.Left == nil && n.Right != nil && n.Right.Value == value {
+		n.Right = findSuccessor(n.Right)
+		return n
+	}
+
+	if value < n.Value {
+		n.Left = deleteNode(n.Left, value)
+	} else {
+		n.Right = deleteNode(n.Right, value)
+	}
+	return n
+}
+
+func findSuccessor(n *node) *node {
+	if n.Left == nil && n.Right == nil {
+		return nil
+	} else if n.Left != nil && n.Right == nil {
+		successor := n.Left
+		n.Left = nil
+		return successor
+	} else if n.Left == nil && n.Right != nil {
+		successor := n.Right
+		n.Right = nil
+		return successor
+	}
+
+	minNode := findMin(n.Right)
+	n.Right = deleteNode(n.Right, minNode.Value)
+	successor := &node{Value: minNode.Value, Left: n.Left, Right: n.Right}
+	n.Left, n.Right = nil, nil
+	return successor
+}
+
+func findMin(n *node) *node {
+	if n == nil {
+		return nil
+	}
+	if n.Left == nil {
+		return n
+	}
+
+	return findMin(n.Left)
+}
+
+func insert(n *node, value uint) *node {
+	if n == nil {
+		return &node{Value: value}
+	}
+
+	if n.Value > value {
+		n.Left = insert(n.Left, value)
+	} else if n.Value < value {
+		n.Right = insert(n.Right, value)
+	}
+	return n
+}
+
+func search(n *node, value uint) *node {
+	if n == nil {
+		return nil
+	}
+
+	if n.Value == value {
+		return n
+	} else if n.Value > value {
+		return search(n.Left, value)
+	} else {
+		return search(n.Right, value)
+	}
+}
---
+++ chapter-three/binpacking/binpacking_test.go
@@ -0,0 +1,179 @@
+package binpacking
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/teleivo/algorithm-design-manual/assert"
+	"github.com/teleivo/algorithm-design-manual/require"
+)
+
+func TestFindBestFit(t *testing.T) {
+	objects := []uint{1, 5, 7, 3, 4}
+
+	assert.Equals(t, FindBestFit(objects, 10), uint(2))
+}
+
+func FuzzFindBestFit(f *testing.F) {
+	f.Add(uint(1), uint(5), uint(7), uint(3), uint(4))
+
+	f.Fuzz(func(t *testing.T, a uint, b uint, c uint, d uint, e uint) {
+		objects := []uint{a, b, c, d, e}
+		var capacity uint = 10
+		total := a + b + c + d + e
+		want := total / capacity
+
+		got := FindBestFit(objects, capacity)
+
+		assert.Equals(t, got, want)
+	})
+}
+
+func TestFindSmallestLargerThan(t *testing.T) {
+	n := New(2, 1, 6, 4, 3, 5, 8, 7)
+
+	t.Run("Found", func(t *testing.T) {
+		got := findSmallestLargerThan(n, 6)
+		assert.Equals(t, got.Value, uint(6))
+
+		got = findSmallestLargerThan(n, 5)
+		assert.Equals(t, got.Value, uint(5))
+
+		got = findSmallestLargerThan(n, 4)
+		assert.Equals(t, got.Value, uint(4))
+
+		got = findSmallestLargerThan(n, 3)
+		assert.Equals(t, got.Value, uint(3))
+	})
+
+	got := findSmallestLargerThan(n, 9)
+	if got != nil {
+		t.Fatalf(fmt.Sprintf("got %v want nil instead", got))
+	}
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("RootWithOnlyLeftChild", func(t *testing.T) {
+		n1 := New(5, 2, 1)
+
+		got := deleteNode(n1, 5)
+
+		want := New(2, 1)
+		assert.EqualValues(t, got, want)
+	})
+	t.Run("RootWithOnlyRightChild", func(t *testing.T) {
+		n1 := New(5, 7, 9)
+
+		got := deleteNode(n1, 5)
+
+		want := New(7, 9)
+		assert.EqualValues(t, got, want)
+	})
+	t.Run("RootWithLeftAndRightChild", func(t *testing.T) {
+		n1 := New(5, 2, 10, 7, 8, 11)
+
+		got := deleteNode(n1, 5)
+
+		want := New(7, 2, 10, 8, 11)
+		assert.EqualValues(t, got, want)
+	})
+	t.Run("LeftParentWithOnlyLeftChild", func(t *testing.T) {
+		n1 := New(5, 2, 1)
+
+		got := deleteNode(n1, 2)
+
+		want := New(5, 1)
+		assert.EqualValues(t, got, want)
+	})
+	t.Run("LeftParentWithOnlyRightChild", func(t *testing.T) {
+		n1 := New(5, 2, 3)
+
+		got := deleteNode(n1, 2)
+
+		want := New(5, 3)
+		assert.EqualValues(t, got, want)
+	})
+	t.Run("LeftParentWithLeftAndRightChild", func(t *testing.T) {
+		n1 := New(10, 3, 12, 8, 6, 5, 7, 9)
+
+		got := deleteNode(n1, 3)
+
+		want := New(10, 12, 8, 6, 5, 7, 9)
+		assert.EqualValues(t, got, want)
+	})
+	t.Run("LeftLeafNode", func(t *testing.T) {
+		n1 := New(5, 2, 1)
+
+		got := deleteNode(n1, 1)
+
+		want := New(5, 2)
+		assert.EqualValues(t, got, want)
+	})
+	t.Run("RightParentWithOnlyLeftChild", func(t *testing.T) {
+		n1 := New(5, 7, 6)
+
+		got := deleteNode(n1, 7)
+
+		want := New(5, 6)
+		assert.EqualValues(t, got, want)
+	})
+	t.Run("RightParentWithOnlyRightChild", func(t *testing.T) {
+		n1 := New(5, 7, 9)
+
+		got := deleteNode(n1, 7)
+
+		want := New(5, 9)
+		assert.EqualValues(t, got, want)
+	})
+	t.Run("RightParentWithLeftAndRightChild", func(t *testing.T) {
+		n1 := New(5, 3, 20, 15, 25, 18, 16, 19, 24, 28)
+
+		got := deleteNode(n1, 20)
+
+		want := New(5, 3, 24, 15, 25, 18, 16, 19, 28)
+		assert.EqualValues(t, got, want)
+	})
+	t.Run("RightLeafNode", func(t *testing.T) {
+		n1 := New(5, 7, 9)
+
+		got := deleteNode(n1, 9)
+
+		want := New(5, 7)
+		assert.EqualValues(t, got, want)
+	})
+	t.Run("NodeNotFound", func(t *testing.T) {
+		n1 := New(5, 7, 9)
+
+		got := deleteNode(n1, 13)
+
+		want := New(5, 7, 9)
+		assert.EqualValues(t, got, want)
+	})
+}
+
+func TestFindMin(t *testing.T) {
+	t.Run("FullTree", func(t *testing.T) {
+		n1 := New(5, 3, 20, 15, 25, 18, 16, 7, 19, 1, 4, 24, 0, 28)
+
+		got := findMin(n1)
+
+		require.NotNil(t, got)
+		assert.Equals(t, got.Value, uint(0))
+	})
+	t.Run("SingleNode", func(t *testing.T) {
+		n1 := New(5)
+
+		got := findMin(n1)
+
+		require.NotNil(t, got)
+		assert.Equals(t, got.Value, uint(5))
+	})
+	t.Run("Nil", func(t *testing.T) {
+		t.Skip("fix asserting nil")
+		n1 := New()
+
+		got := findMin(n1)
+
+		require.Nil(t, got)
+	})
+}
